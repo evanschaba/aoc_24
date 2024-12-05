83054a5897dba561d3124f4a0a25769ba685c8c2
evanschaba <evanschaba@gmail.com>
2024-12-05 11:46:09 +0200

    ☑️ ➕doc md

--- day-05/target/debug/deps/day_05-65ce8c6e55c4efcb.d
+++
@@ -1,5 +0,0 @@
-/Users/deltd3v/Desktop/workspaces/aoc_24/day-05/target/debug/deps/libday_05-65ce8c6e55c4efcb.rmeta: src/main.rs
-
-/Users/deltd3v/Desktop/workspaces/aoc_24/day-05/target/debug/deps/day_05-65ce8c6e55c4efcb.d: src/main.rs
-
-src/main.rs:
