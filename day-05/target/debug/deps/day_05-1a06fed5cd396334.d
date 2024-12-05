f33b980d42f36cfc796b75fb1b71dd4ab4c203e4
evanschaba <evanschaba@gmail.com>
2024-12-05 11:45:34 +0200

    ☑️ ➕doc md

---
+++ day-05/target/debug/deps/day_05-1a06fed5cd396334.d
@@ -0,0 +1,5 @@
+/Users/deltd3v/Desktop/workspaces/aoc_24/day-05/target/debug/deps/libday_05-1a06fed5cd396334.rmeta: src/main.rs
+
+/Users/deltd3v/Desktop/workspaces/aoc_24/day-05/target/debug/deps/day_05-1a06fed5cd396334.d: src/main.rs
+
+src/main.rs:
---
+++ day-05/target/debug/deps/day_05-65ce8c6e55c4efcb.d
@@ -0,0 +1,5 @@
+/Users/deltd3v/Desktop/workspaces/aoc_24/day-05/target/debug/deps/libday_05-65ce8c6e55c4efcb.rmeta: src/main.rs
+
+/Users/deltd3v/Desktop/workspaces/aoc_24/day-05/target/debug/deps/day_05-65ce8c6e55c4efcb.d: src/main.rs
+
+src/main.rs:
